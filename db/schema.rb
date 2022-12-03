079336655b4148f0a97b6c2d7926ee4d97034d21
ysk91 <ysk91.engineer@gmail.com>
2022-12-03 14:36:32 +0900

    articleモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_034417) do
+ActiveRecord::Schema.define(version: 2022_12_03_052944) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "auther", null: false
+    t.datetime "dalete_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name", null: false
